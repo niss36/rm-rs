3bc1816e7c037ea13a518f83cbecb53066e99aa3
Nissim Chekroun <nissim.chekroun@gmail.com>
2024-03-31 18:32:54 +0100

    feat: error when trying to delete the working directory or any of its parents

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use core::{remove, RemoveMode};
-use std::{path::PathBuf, process::ExitCode};
+use std::{io, path::PathBuf, process::ExitCode};
 
 use clap::Parser;
 
 mod core;
+mod safety;
+
+use core::{remove, RemoveMode};
+use safety::does_any_path_contain_working_directory;
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -32,15 +35,19 @@ impl Cli {
     }
 }
 
-fn main() -> ExitCode {
+fn main() -> io::Result<ExitCode> {
     let args = Cli::parse();
+
+    if does_any_path_contain_working_directory(&args.files)? {
+        return Ok(ExitCode::FAILURE);
+    }
 
     let mode = args.get_remove_mode();
 
     let result = remove(mode, args.files, args.ignore_not_found);
 
     match result {
-        Ok(_) => ExitCode::SUCCESS,
-        Err(_) => ExitCode::FAILURE,
+        Ok(_) => Ok(ExitCode::SUCCESS),
+        Err(_) => Ok(ExitCode::FAILURE),
     }
 }
---
+++ src/safety.rs
@@ -0,0 +1,99 @@
+use std::{
+    env, io,
+    path::{Path, PathBuf},
+};
+
+use path_absolutize::Absolutize;
+
+fn contains_working_directory(path: &Path, working_directory: &Path) -> io::Result<bool> {
+    let absolute_path = path.absolutize_from(working_directory)?;
+
+    Ok(working_directory.starts_with(absolute_path))
+}
+
+fn get_paths_containing_working_directory(
+    paths: &[PathBuf],
+    working_directory: &Path,
+) -> io::Result<Vec<PathBuf>> {
+    let mut results = vec![];
+
+    for path in paths {
+        if contains_working_directory(path, working_directory)? {
+            results.push(path.clone());
+        }
+    }
+
+    Ok(results)
+}
+
+pub fn does_any_path_contain_working_directory(paths: &[PathBuf]) -> io::Result<bool> {
+    let working_directory = env::current_dir()?;
+
+    let offending_paths = get_paths_containing_working_directory(paths, &working_directory)?;
+
+    if offending_paths.is_empty() {
+        return Ok(false);
+    }
+
+    for path in offending_paths {
+        eprintln!(
+            "Cannot delete '{}': Contains the working directory",
+            path.display()
+        );
+    }
+
+    return Ok(true);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use super::*;
+
+    #[test]
+    fn contains_working_directory_returns_true_if_equal() {
+        let working_directory = env::current_dir().unwrap();
+
+        let result = contains_working_directory(&working_directory, &working_directory).unwrap();
+
+        assert!(result);
+    }
+
+    #[test]
+    fn contains_working_directory_returns_true_for_dot() {
+        let working_directory = env::current_dir().unwrap();
+
+        let result = contains_working_directory(Path::new("."), &working_directory).unwrap();
+
+        assert!(result);
+    }
+
+    #[test]
+    fn contains_working_directory_returns_true_for_parent() {
+        let working_directory = env::current_dir().unwrap();
+
+        let result =
+            contains_working_directory(Path::new("foo/../.."), &working_directory).unwrap();
+
+        assert!(result);
+    }
+
+    #[test]
+    fn contains_working_directory_returns_false_for_child() {
+        let working_directory = env::current_dir().unwrap();
+
+        let result = contains_working_directory(Path::new("./foo"), &working_directory).unwrap();
+
+        assert!(!result);
+    }
+
+    #[test]
+    fn contains_working_directory_returns_false_for_sibling() {
+        let working_directory = env::current_dir().unwrap();
+
+        let result = contains_working_directory(Path::new("../foo"), &working_directory).unwrap();
+
+        assert!(!result);
+    }
+}
