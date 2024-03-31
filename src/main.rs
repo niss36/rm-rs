767b7ddb7d60438e98843b37e3ee52c9dc52f39d
Nissim Chekroun <nissim.chekroun@gmail.com>
2024-03-31 11:41:59 +0100

    feat: add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// Attempt to remove directories as well as other types of files.
+    #[clap(short)]
+    directories: bool,
+
+    /// Recursively remove directories and the files they contain. This implies the -d option
+    #[clap(short)]
+    recursive: bool,
+
+    files: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("{args:#?}");
 }
